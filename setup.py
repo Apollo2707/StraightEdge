230140c7a61701ef816dd14dea4f215a5a7527b0
Build a better bonsai <neumann6994@gmail.com>
2018-02-26 14:24:59 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,22 @@ from setuptools import setup
 
 setup(name='StraightEdge',
       version='0.1',
-      description='Personal webscrapper tool',
+      description='Webscrapper',
+      long_description='Personal Built WebScrapper',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3. :: Only,
+        'Topic :: Internet :: WWW/HTTP',
+      ],
+      keywords='webscrapper web scrapper text',
       url='https://github.com/Apollo2707/StraightEdge',
       author='Andrew Neumann',
       author_email='neumann6994@gmail.com',
       license='MIT',
-      packages=['StraightEdge'],
+      packages=['text'],
+      install_requires=[
+          'markdown',
+      ],
+      include_package_data=True,
       zip_safe=False)
